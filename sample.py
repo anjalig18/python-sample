7b91d2a941363a2996a454e47df0d2c88a57c23e
Anjali gupta <anjalig1810@gmail.com>
2021-12-29 20:14:34 +0530

    sample.py

---
+++ sample.py
@@ -0,0 +1,16 @@
+a=5
+b=7
+print("sum of 5 and 7 is",5+7)
+print("multiply of 5 and 7 is ",5*7)
+print("divide of 5 and 7 is",5/7)
+print(int (5/7))
+print(float(5/7))
+name="Anjali gupta"
+age=19
+marks=82.55
+print(name)
+print(age)
+print(marks)
+
+
+
